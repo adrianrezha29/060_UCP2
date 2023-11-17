6e4cfcb547814eb4126500ace33fe92aadcb0075
adrianrezha29 <adrianrezha29@gmail.com>
2023-11-17 15:07:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ucp2/Data/UIState.kt
@@ -0,0 +1,10 @@
+package com.example.ucp2.Data
+
+data class UIState(
+    val nama : String= "",
+    val nim : String = "",
+    val konsentrasi : String = "",
+    val judul : String = "",
+    val dospem1 : String = "",
+    val dospem2 : String = ""
+)
